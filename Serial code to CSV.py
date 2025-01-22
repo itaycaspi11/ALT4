dcc8b86ac98f66bb08445a912c2cb3906604a29f
itaycaspi11 <icaspi20@hcetss.ie>
2025-01-22 11:53:43 +0000

    Update Serial code to CSV.py

--- Serial code to CSV.py
+++ Serial code to CSV.py
@@ -3,25 +3,25 @@ import serial
 ser=serial.Serial() #shortens serial function to ser
 ser.baudrate=115200 #needed to get correct signal from microbit
 
-ser.port='COM3' #found in command prompt - chgport or device manager
+ser.port='COM6' #found in command prompt - chgport or device manager
 
 print("Type Ctrl + C to safety exit")
 ser.open() #opens serial port
 
 #initialise variables
-sound=" "
+temperature=" "
 
 while True:
     data=str(ser.readline())
     data=data[2:14].replace(" ", "")
-    sound=data.replace("Sound:", "")
+    sound=data.replace("Temperature:", "")
         
             
     #opening csv file
-    file=open("Microbitsound2.csv", "a")
+    file=open("Microbittemprature.csv", "a")
         
     #writing the data
-    file.write(sound+",")
+    file.write(temperature+",")
     file.close()
         
         
